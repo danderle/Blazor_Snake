4a236d52dcf4991ce5c26ad4a731d9c073264297
danderle <danderle86@gmail.com>
2023-06-19 14:45:11 +0200

    added HighScore Api project

---
+++ HighScoreApi/Api.cs
@@ -0,0 +1,39 @@
+﻿using MongoDbAccess.Data;
+using MongoDbAccess.DbAccess;
+
+namespace HighScoreApi
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapGet("/HighScores", GetHighScores);
+            app.MapPost("/HighScores", CreateHighScore);
+        }
+
+        private static async Task<IResult> GetHighScores(IDataAccess data)
+        {
+            try
+            {
+                return Results.Ok(await data.GetHighScoresAsync());
+            }
+            catch(Exception e)
+            {
+                return Results.Problem(e.Message);
+            }
+        }
+
+        private static async Task<IResult> CreateHighScore(HighScoreModel highScore, IDataAccess data)
+        {
+            try
+            {
+                await data.CreateHighScoreAsync(highScore);
+                return Results.Ok();
+            }
+            catch (Exception e)
+            {
+                return Results.Problem(e.Message);
+            }
+        }
+    }
+}
