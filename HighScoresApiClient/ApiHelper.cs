c63109ac674abeb5fb8bf7a864b8ee6e544aebf5
danderle <danderle86@gmail.com>
2023-06-20 11:02:28 +0200

    Added the HighScoresApiClient to call the external api

---
+++ HighScoresApiClient/ApiHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+
+namespace HighScoresApiClient;
+
+public static class ApiHelper
+{
+    public static HttpClient Client { get; private set; }
+
+    static ApiHelper()
+    {
+        InitializeClient();
+    }
+
+    private static void InitializeClient()
+    {
+        Client = new HttpClient();
+        Client.BaseAddress = new Uri("https://localhost:7089");
+        Client.DefaultRequestHeaders.Accept.Clear();
+        Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+}
---
+++ HighScoresApiClient/HighScoresRequests.cs
@@ -0,0 +1,39 @@
+﻿using MongoDbData.Data;
+
+namespace HighScoresApiClient;
+
+public class HighScoresRequests
+{
+    public const string Url = "/HighScores";
+
+    public static async Task<List<HighScoreModel>> LoadHighScores()
+    {
+        using(HttpResponseMessage response = await ApiHelper.Client.GetAsync(Url))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var list = await response.Content.ReadAsAsync<List<HighScoreModel>>();
+                return list;
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+
+    public static async Task<bool> SaveHighScore(HighScoreModel highscore)
+    {
+        using (HttpResponseMessage response = await ApiHelper.Client.PostAsJsonAsync(Url, highscore))
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+            else
+            {
+                throw new Exception(response.ReasonPhrase);
+            }
+        }
+    }
+}
